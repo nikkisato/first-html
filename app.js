520a4e9e6bba114d7519b79062bf8f8832a7d6d7
Nikki <nikki.satopdx@gmail.com>
2019-09-18 21:00:29 -0700

    new app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+function showAlert () {alert ('you clicked the button');}
+
+function logHello() {console.log('Hello');}
+
+
+
+
+const dog = {
+name: 'fido',
+color: 'brown',
+weight: 500,
+
+}
+
+console.log(dog.name);
+
+dog.name = 'rover';
+
+console.log(dog.name);
+
+console.log(dog.weight);
+
+if(dog.name === 'rover') {console.log('red rover, come over')}
+else if (dog.name === 'spot') {console.log('you are a normal dog')}
+else if (dog.name === 'timothy') {console.log('that is a person name')}
+else { console.log('you are a normal person ')}
+
+console.log('I love corgi')
+
+//count up to ten
+const veggies = ['carrot', 'potato','leek']
+
+for (let i = 0; i <= 2; i++) {
+console.log(i);
+console.log(veggies[i]);
+
+}
+
+console.log('I love corgis')
